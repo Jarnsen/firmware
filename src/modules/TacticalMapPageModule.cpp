3772fe01f79c344011642e95e21977c8559cbe07
Jarnsen <jarno.karsch@vodafone.de>
2026-07-22 14:04:18 +0200

    feat: publish full-color tactical map frames

--- src/modules/TacticalMapPageModule.cpp
+++ src/modules/TacticalMapPageModule.cpp
@@ -7,6 +7,9 @@
 #include "TacticalMapMath.h"
 #include "graphics/ScreenFonts.h"
 #include "graphics/SharedUIDisplay.h"
+#include "graphics/TFTColorRegions.h"
+#include "graphics/TFTPalette.h"
+#include "graphics/TacticalDisplayMirror.h"
 #include "graphics/draw/UIRenderer.h"
 
 #include <cstdio>
@@ -18,12 +21,29 @@ void drawCross(OLEDDisplay *display, int16_t x, int16_t y)
     display->drawLine(x - 3, y, x + 3, y);
     display->drawLine(x, y - 3, x, y + 3);
     display->drawCircle(x, y, 2);
+    graphics::drawTacticalColorLine(x - 3, y, x + 3, y, graphics::TFTPalette::Green);
+    graphics::drawTacticalColorLine(x, y - 3, x, y + 3, graphics::TFTPalette::Green);
+    graphics::drawTacticalColorCircle(x, y, 2, graphics::TFTPalette::Green);
+#if GRAPHICS_TFT_COLORING_ENABLED
+    graphics::registerTFTColorRegionDirect(x - 4, y - 4, 9, 9, graphics::TFTPalette::Green, graphics::getThemeBodyBg());
+#endif
 }
 
 void drawTarget(OLEDDisplay *display, int16_t x, int16_t y)
 {
     display->drawCircle(x, y, 3);
     display->setPixel(x, y);
+    graphics::drawTacticalColorCircle(x, y, 3, graphics::TFTPalette::Red);
+    graphics::setTacticalColorPixel(x, y, graphics::TFTPalette::Red);
+#if GRAPHICS_TFT_COLORING_ENABLED
+    graphics::registerTFTColorRegionDirect(x - 4, y - 4, 9, 9, graphics::TFTPalette::Red, graphics::getThemeBodyBg());
+#endif
+}
+
+void finishColorFrame(OLEDDisplay *display)
+{
+    graphics::overlayTacticalMonoBuffer(display, graphics::TFTPalette::White, graphics::TFTPalette::Black);
+    graphics::publishTacticalColorFrame();
 }
 } // namespace
 
@@ -86,6 +106,8 @@ void TacticalMapPageModule::drawFrame(OLEDDisplay *display, OLEDDisplayUiState *
     if (!display || !nodeDB)
         return;
 
+    graphics::beginTacticalColorFrame(display->getWidth(), display->getHeight(), graphics::TFTPalette::Black);
+
     display->clear();
     display->setTextAlignment(TEXT_ALIGN_LEFT);
     display->setFont(FONT_SMALL);
@@ -99,11 +121,12 @@ void TacticalMapPageModule::drawFrame(OLEDDisplay *display, OLEDDisplayUiState *
     display->drawRect(mapLeft, mapTop, mapWidth, mapHeight);
     JTMapRenderer::Bounds bounds;
     const bool haveMap = JTMapRenderer::draw(display, JTMapRenderer::DEFAULT_MAP_PATH, mapLeft + 1, mapTop + 1,
-                                               mapWidth - 2, mapHeight - 2, &bounds,
-                                               JTMapRenderer::Theme::TACTICAL_NIGHT);
+                                              mapWidth - 2, mapHeight - 2, &bounds,
+                                              JTMapRenderer::Theme::TACTICAL_NIGHT);
     if (!haveMap) {
         display->drawString(mapLeft + 18, mapTop + 15, "NO JTMAP");
         display->drawString(mapLeft + 8, mapTop + 29, "Install Friesenheim");
+        finishColorFrame(display);
         return;
     }
 
@@ -114,7 +137,7 @@ void TacticalMapPageModule::drawFrame(OLEDDisplay *display, OLEDDisplayUiState *
 
     meshtastic_PositionLite ownPosition;
     const bool haveOwn = nodeDB->copyNodePosition(nodeDB->getNodeNum(), ownPosition) &&
-                          TacticalMapMath::isValidCoordinate(ownPosition.latitude_i, ownPosition.longitude_i);
+                         TacticalMapMath::isValidCoordinate(ownPosition.latitude_i, ownPosition.longitude_i);
     const bool ownInside = haveOwn && JTMapRenderer::contains(bounds, ownPosition.latitude_i, ownPosition.longitude_i);
     int16_t ownX = 0;
     int16_t ownY = 0;
@@ -134,14 +157,25 @@ void TacticalMapPageModule::drawFrame(OLEDDisplay *display, OLEDDisplayUiState *
     if (targetInside) {
         const int16_t targetX = JTMapRenderer::projectX(bounds, targetPosition.longitude_i, mapLeft + 1, mapWidth - 2);
         const int16_t targetY = JTMapRenderer::projectY(bounds, targetPosition.latitude_i, mapTop + 1, mapHeight - 2);
-        if (ownInside)
+        if (ownInside) {
             display->drawLine(ownX, ownY, targetX, targetY);
+            graphics::drawTacticalColorLine(ownX, ownY, targetX, targetY, graphics::TFTPalette::Yellow);
+#if GRAPHICS_TFT_COLORING_ENABLED
+            const int16_t routeLeft = std::min(ownX, targetX) - 1;
+            const int16_t routeTop = std::min(ownY, targetY) - 1;
+            graphics::registerTFTColorRegionDirect(routeLeft, routeTop, std::abs(targetX - ownX) + 3,
+                                                   std::abs(targetY - ownY) + 3, graphics::TFTPalette::Yellow,
+                                                   graphics::getThemeBodyBg());
+#endif
+        }
         drawTarget(display, targetX, targetY);
         display->drawString(mapLeft + mapWidth - 34, mapTop + 1, targetName);
     }
 
     if (ownInside)
         drawCross(display, ownX, ownY);
+
+    finishColorFrame(display);
 }
 
 #endif
