5d174f1b38d7c42851b5b45545fb881fbe7acdc9
Jarnsen <jarno.karsch@vodafone.de>
2026-08-22 16:17:04 +0200

    Add V3 runtime health interface

---
+++ src/infrastructure/HeltecV3Runtime.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <cstdint>
+
+bool heltecV3RuntimeRoleEnabled();
+bool heltecV3RuntimeServiceActive();
+bool heltecV3RuntimeUsbMaintenanceActive();
+const char *heltecV3RuntimeStateText();
+const char *heltecV3RuntimeBleStateText();
+uint32_t heltecV3RuntimeServiceRemainingSecs();
