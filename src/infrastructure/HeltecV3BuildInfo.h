241bee6098e8719de95d7b8ced32d095a6d5e240
Jarnsen <jarno.karsch@vodafone.de>
2026-08-22 16:17:16 +0200

    Add V3 build identity header

---
+++ src/infrastructure/HeltecV3BuildInfo.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#if __has_include("infrastructure/HeltecV3BuildGenerated.h")
+#include "infrastructure/HeltecV3BuildGenerated.h"
+#else
+#define JARNSEN_V3_BUILD_SHA "unknown"
+#endif
