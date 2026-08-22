4386032049acb26d5edebbec7ac3111ea8973cad
Jarnsen <jarno.karsch@vodafone.de>
2026-08-22 10:34:14 +0200

    Expose Tracker service setting selectors

--- src/vehicle/TrackerServiceSettings.h
+++ src/vehicle/TrackerServiceSettings.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <stddef.h>
 #include <stdint.h>
 
 #if defined(HELTEC_TRACKER_V1_1)
@@ -17,6 +18,12 @@ uint16_t trackerSmartIntervalSecs();
 uint16_t trackerParkIntervalMinutes();
 uint32_t trackerParkIntervalSecs();
 uint32_t trackerEffectiveParkIntervalSecs();
+void trackerFormatParkInterval(char *out, size_t outSize);
+
+bool trackerSetMotionSensitivityIndex(uint8_t index);
+bool trackerSetSmartDistanceM(uint16_t meters);
+bool trackerSetSmartIntervalSecs(uint16_t seconds);
+bool trackerSetParkIntervalMinutes(uint16_t minutes);
 
 void trackerCycleMotionSensitivity();
 void trackerCycleSmartDistance();
