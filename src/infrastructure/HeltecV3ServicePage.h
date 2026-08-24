b8a761e2a08ac760541670268af5005af3860d87
Jarnsen <jarno.karsch@vodafone.de>
2026-08-24 09:29:57 +0200

    Improve V3 service pages and log export

--- src/infrastructure/HeltecV3ServicePage.h
+++ src/infrastructure/HeltecV3ServicePage.h
@@ -10,6 +10,7 @@ struct OLEDDisplayUiState;
 bool heltecV3ServicePageEnabled();
 #if HAS_SCREEN
 void heltecV3ServicePageDrawFrame(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y);
+void heltecV3ServiceSetupPageDrawFrame(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y);
 #endif
 void heltecV3ServicePageRefresh();
 bool heltecV3ServicePageRecentlyVisible();
