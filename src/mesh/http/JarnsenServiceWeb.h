36ec0cf79950b81b30014ad164e00b57fbac0d24
Jarnsen <jarno.karsch@vodafone.de>
2026-08-26 21:45:40 +0200

    V3: expose deferred WLAN start request

--- src/mesh/http/JarnsenServiceWeb.h
+++ src/mesh/http/JarnsenServiceWeb.h
@@ -5,6 +5,7 @@
 #if defined(ARCH_ESP32) && HAS_WIFI && (defined(_VARIANT_HELTEC_V3) || defined(HELTEC_TRACKER_V1_1))
 
 bool jarnsenServiceWebStart();
+bool jarnsenServiceWebRequestStart();
 void jarnsenServiceWebStop();
 void jarnsenServiceWebPump();
 bool jarnsenServiceWebActive();
@@ -13,9 +14,17 @@ const char *jarnsenServiceWebPassword();
 const char *jarnsenServiceWebAddress();
 const char *jarnsenServiceWebLastError();
 
+#if defined(_VARIANT_HELTEC_V3) && defined(JARNSEN_SERVICE_WEB_DEFER_FROM_UI)
+#define jarnsenServiceWebStart() jarnsenServiceWebRequestStart()
+#endif
+
 #else
 
 inline bool jarnsenServiceWebStart()
+{
+    return false;
+}
+inline bool jarnsenServiceWebRequestStart()
 {
     return false;
 }
