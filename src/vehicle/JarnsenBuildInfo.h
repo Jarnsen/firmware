c23a9c1e17ae3b2070c4fb1df79d60a7af79011e
Jarnsen <jarno.karsch@vodafone.de>
2026-08-28 17:11:30 +0200

    Version Tracker firmware as v1.1.0

--- src/vehicle/JarnsenBuildInfo.h
+++ src/vehicle/JarnsenBuildInfo.h
@@ -1,9 +1,11 @@
 #pragma once
 
-#define JARNSEN_FIRMWARE_VERSION "JARN-MESH 1.1"
-
 #if __has_include("JarnsenBuildGenerated.h")
 #include "JarnsenBuildGenerated.h"
+#endif
+
+#ifndef JARNSEN_FIRMWARE_VERSION
+#define JARNSEN_FIRMWARE_VERSION "JARN-MESH v1.1.0"
 #endif
 
 #ifndef JARNSEN_BUILD_SHA
