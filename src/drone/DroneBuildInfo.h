7c74912b4f7c38bc6adee84007381f444cb54504
Jarnsen <jarno.karsch@vodafone.de>
2026-08-28 18:25:47 +0200

    Set reconstructed Drone firmware version to v1.5.1

---
+++ src/drone/DroneBuildInfo.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#if __has_include("drone/DroneBuildGenerated.h")
+#include "drone/DroneBuildGenerated.h"
+#endif
+
+#ifndef JARNSEN_DRONE_FIRMWARE_SEMVER
+#define JARNSEN_DRONE_FIRMWARE_SEMVER "v1.5.1"
+#endif
+
+#ifndef JARNSEN_DRONE_FIRMWARE_VERSION
+#define JARNSEN_DRONE_FIRMWARE_VERSION "JARN-DRONE v1.5.1"
+#endif
+
+#ifndef JARNSEN_DRONE_BUILD_SHA
+#define JARNSEN_DRONE_BUILD_SHA "local"
+#endif
+
+#ifndef JARNSEN_DRONE_BUILD_NUMBER
+#define JARNSEN_DRONE_BUILD_NUMBER 0
+#endif
