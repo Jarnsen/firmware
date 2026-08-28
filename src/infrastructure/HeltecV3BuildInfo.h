29a755ee38daef181a887e86183d039ab07cfa73
Jarnsen <jarno.karsch@vodafone.de>
2026-08-28 18:23:56 +0200

    Set reconstructed V3 firmware version to v1.9.1

--- src/infrastructure/HeltecV3BuildInfo.h
+++ src/infrastructure/HeltecV3BuildInfo.h
@@ -2,6 +2,20 @@
 
 #if __has_include("infrastructure/HeltecV3BuildGenerated.h")
 #include "infrastructure/HeltecV3BuildGenerated.h"
-#else
-#define JARNSEN_V3_BUILD_SHA "unknown"
+#endif
+
+#ifndef JARNSEN_V3_FIRMWARE_SEMVER
+#define JARNSEN_V3_FIRMWARE_SEMVER "v1.9.1"
+#endif
+
+#ifndef JARNSEN_V3_FIRMWARE_VERSION
+#define JARNSEN_V3_FIRMWARE_VERSION "JARN-MESH V3 v1.9.1"
+#endif
+
+#ifndef JARNSEN_V3_BUILD_SHA
+#define JARNSEN_V3_BUILD_SHA "local"
+#endif
+
+#ifndef JARNSEN_V3_BUILD_NUMBER
+#define JARNSEN_V3_BUILD_NUMBER 0
 #endif
