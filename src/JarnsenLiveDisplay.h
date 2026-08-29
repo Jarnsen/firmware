ce5d09155de3b197feeac6340b65171712138033
Jarnsen <jarno.karsch@vodafone.de>
2026-08-29 08:07:30 +0200

    Extract shared live display into unified core

--- src/JarnsenLiveDisplay.h
+++ src/JarnsenLiveDisplay.h
@@ -1,17 +1,5 @@
 #pragma once
 
-#include <cstddef>
-#include <cstdint>
-
-struct JarnsenLiveFrameInfo {
-    uint8_t width = 0;
-    uint8_t height = 0;
-    uint16_t sequence = 0;
-    bool screenOn = false;
-};
-
-void jarnsenLiveSetActive(bool active);
-bool jarnsenLiveIsActive();
-bool jarnsenLiveHandleCommand(const char *command);
-void jarnsenLiveRequestRender();
-size_t jarnsenLiveCapture(uint8_t *buffer, size_t capacity, JarnsenLiveFrameInfo &info);
+// Compatibility include during the unified-core migration. New code should
+// include jarnsen/core/display/JarnsenLiveDisplay.h directly.
+#include "jarnsen/core/display/JarnsenLiveDisplay.h"
