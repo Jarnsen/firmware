6b1eabb75e2347150bb1b68eaeab818844aa7882
Jarnsen <jarno.karsch@vodafone.de>
2026-08-31 10:01:42 +0200

    Move runtime status provider into common status layer

---
+++ src/jarnsen/core/status/JarnsenStatusProvider.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "jarnsen/core/status/JarnsenNodeStatus.h"
+
+namespace jarnsen
+{
+
+using PeripheralCapabilitiesProvider = PeripheralCapabilities (*)();
+using DeviceRoleProvider = DeviceRole (*)();
+
+struct StatusProviderHooks {
+    PeripheralCapabilitiesProvider peripherals = nullptr;
+    DeviceRoleProvider activeRole = nullptr;
+};
+
+// Hardware/role adapters register only the runtime facts they own. Consumers
+// never need to know which board supplied those facts.
+void setStatusProviderHooks(const StatusProviderHooks &hooks);
+StatusProviderHooks statusProviderHooks();
+
+NodeStatusSnapshot readNodeStatus(const HardwareRoleProfile &profile);
+
+} // namespace jarnsen
