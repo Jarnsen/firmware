ec28d646e2dd01546aad7971349e9e3f7a07072d
Jarnsen <jarno.karsch@vodafone.de>
2026-09-03 20:44:42 +0200

    feat: enable USB node log UI layer

--- tools/jarnsen_mesh_flasher/_build_version.py
+++ tools/jarnsen_mesh_flasher/_build_version.py
@@ -3,3 +3,11 @@ from runtime_config import configure_runtime
 APP_VERSION = "1.0.0-alpha.1 \u00b7 Build DEV"
 
 configure_runtime()
+
+try:
+    import services
+    from usb_log_download import install as install_usb_log_download
+
+    install_usb_log_download(services)
+except Exception:
+    pass
