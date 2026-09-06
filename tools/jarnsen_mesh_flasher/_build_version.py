d9986a9e3f98ea1cf661880f81517bdc52278a34
Jarnsen <jarno.karsch@vodafone.de>
2026-09-06 18:42:51 +0200

    feat: wire radio profiles into flasher runtime

--- tools/jarnsen_mesh_flasher/_build_version.py
+++ tools/jarnsen_mesh_flasher/_build_version.py
@@ -2,10 +2,15 @@ from runtime_config import configure_runtime
 from windows_branding import configure_windows_branding
 from windows_usb_fallback import install as install_windows_usb_fallback
 from manual_board_fallback import install as install_manual_board_fallback
+from radio_profiles import install as install_radio_profiles
+from radio_profiles_ui import install as install_radio_profiles_ui
 
 APP_VERSION = "1.0.0-alpha.2 · Build DEV"
 
 configure_windows_branding()
 configure_runtime()
-install_manual_board_fallback(__import__("services"))
+_services = __import__("services")
+install_manual_board_fallback(_services)
 install_windows_usb_fallback()
+install_radio_profiles(_services)
+install_radio_profiles_ui(_services)
