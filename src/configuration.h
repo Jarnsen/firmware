4a5d903ae96fc6b9ebe7ab4beba9f232cf4eb860
Jarnsen <jarno.karsch@vodafone.de>
2026-09-17 08:15:32 +0200

    fix(firmware): restore VEML7700 address symbol

--- src/configuration.h
+++ src/configuration.h
@@ -274,7 +274,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define QMA6100P_ADDR 0x12
 #define AHT10_ADDR 0x38
 #define RCWL9620_ADDR 0x57
-#define VEML7702_ADDR 0x10
+#define VEML7700_ADDR 0x10
 #define TSL25911_ADDR 0x29
 #define OPT3001_ADDR 0x45
 #define OPT3001_ADDR_ALT 0x44
@@ -306,7 +306,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define BMA423_ADDR 0x19
 #define LSM6DS3_ADDR 0x6A
 #define BMX160_ADDR 0x69
-#define ICM20948_ADDR 0x69
+#define ICM20948_ADDR 0x68
 #define ICM20948_ADDR_ALT 0x68
 #define BHI260AP_ADDR 0x28
 #define BMM150_ADDR 0x13
